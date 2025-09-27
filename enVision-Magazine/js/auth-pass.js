bdc485f8ee88c0d8c218a4c236e624fd6f674c9f
Aws-A <awsalrahim@yahoo.com>
2025-09-26 22:49:50 -0400

    Magazine: Pass Auth

---
+++ enVision-Magazine/js/auth-pass.js
@@ -0,0 +1,39 @@
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-app.js";
+  import { getAuth, onAuthStateChanged } from "https://www.gstatic.com/firebasejs/10.12.2/firebase-auth.js";
+
+  // Firebase config (same as in index.js)
+  const firebaseConfig = {
+    apiKey: "AIzaSyB_aEQJrZILdy2JYl6HbLwT43myJhVJH6g",
+    authDomain: "envision-ed5f3.firebaseapp.com",
+    projectId: "envision-ed5f3",
+    storageBucket: "envision-ed5f3.firebasestorage.app",
+    messagingSenderId: "202691344541",
+    appId: "1:202691344541:web:2f6c93e7f5138adf127c74",
+    measurementId: "G-3NB50YD8F0"
+  };
+
+  const app = initializeApp(firebaseConfig);
+  const auth = getAuth(app);
+
+  // When user is logged in, show their email
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      document.getElementById("userEmail").textContent = user.email;
+    }
+  });
+
+    const writeLink = document.getElementById("writeLink");
+
+  writeLink.addEventListener("click", (e) => {
+    e.preventDefault(); // stop default navigation first
+    const user = auth.currentUser;
+
+    if (user) {
+      // User is logged in → go to article page
+      window.location.href = "writing-article.html";
+    } else {
+      // User not logged in → alert + redirect
+      alert("Please log in to write an article.");
+      window.location.href = "../index.html"; // outside folder
+    }
+  });
